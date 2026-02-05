f48a26025c70a2c3945f8a8e1ff486b0fed9b7a2
gveal <gveal07@outlook.com>
2026-02-05 18:56:16 +0000

    added input to square function

--- Methods/Methods.java
+++ Methods/Methods.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Methods {
     public static void square(int num) {
         System.out.println("The square of " + num + " is " + num*num + ".");
     } 
 
     public static void main(String[] args) {
-        square(5);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to find the square of it: ");
+        int num = scanner.nextInt();
+
+        square(num);
     }
 }
