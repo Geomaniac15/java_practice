f08b6801ceb482ae36dad50ae429eae86f566517
gveal <gveal07@outlook.com>
2026-02-05 17:22:47 +0000

    more practice

---
+++ Loops/Loops.java
@@ -0,0 +1,17 @@
+public class Loops {
+    public static void main(String[] args){
+        /* 
+        For loop
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        */
+
+        int i = 0;
+        while (i < 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ Methods/Methods.java
@@ -0,0 +1,9 @@
+public class Methods {
+    public static void square(int num) {
+        System.out.println("The square of " + num + " is " + num*num + ".");
+    } 
+
+    public static void main(String[] args) {
+        square(5);
+    }
+}
